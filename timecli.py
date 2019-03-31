c3525fa5e19ae0238e4571e1cc48209706993248
Matt <matthew.ruben@sjsu.edu>
2019-03-31 14:19:04 -0700

    Added subtracting functionality

--- timecli.py
+++ timecli.py
@@ -22,23 +22,22 @@ def get_arguments():
     """
     parser = argparse.ArgumentParser()
     # Add time1 argument
-    parser.add_argument(name='time1',
+    parser.add_argument('time1',
                         help='The time to add to or subtract from',
                         type=str,
-                        required=True,)
+                        default='1:10')
 
     # Add operator argument
-    parser.add_argument(name='operator',
+    parser.add_argument('operator',
                         help='Whether to add or subtract the operands',
                         type=str,
-                        required=True,
                         choices=['+', '-'])
 
     # Add time2 argument
-    parser.add_argument(name='time2',
+    parser.add_argument('time2',
                         help='The time to add or subtract from time1',
                         type=str,
-                        required=True)
+                        default='3:30')
 
     # Add verbose option
     parser.add_argument('-v', '--verbose',
@@ -54,24 +53,62 @@ def get_arguments():
     return time1, operator, time2, verbose
 
 
-
-
-
 class Timecli:
     """
     Class that implements a command line application that lets users
     add and subtract times
     """
+    time1: str
+    time2: str
+    operator: str
+    verbose: bool
 
-    def main(self):
+    def __init__(self):
+        self.time1, self.operator, self.time2, self.verbose = get_arguments()
+        self.parse()
 
-    def __init__(self, time1, operator, time2, verbose):
-        self.time1 = time1
-        self.operator = operator
-        self.time2 = time2
-        self.verbose = verbose
+    def parse(self):
+        hour1, min1 = self.time1.split(':', 2)
+        hour2, min2 = self.time2.split(':', 2)
+        if len(min2) == 4:
+            ampm = min2[2:]
+            min2 = min2[:-2]
+            print(min2)
+            print(ampm)
+        if self.operator == '-':
+            hours = int(hour1) - int(hour2)
+            mins = int(min1) - int(min2)
+            if mins < 0:
+                temp = abs(mins)
+                hours -= 1
+                mins = 60 - temp
+
+        print(f'{hours}:{mins}')
+
+
+class Time:
+    hour: int
+    minute: int
+    ampm: str
+
+    def __init__(self, string):
+        self.hour = string[0:2]
+        self.minute = string[3:5]
+        if len(string) > 5:
+            self.ampm = string[5:]
+        else:
+            self.ampm = None
+
+    def __str__(self):
+        if self.ampm:
+            return f'{self.hour}:{self.minute}{self.ampm}'
+        else:
+            return f'{self.hour}:{self.minute}'
 
 
 
+def main():
+    Timecli()
 
-
+if __name__ == '__main__':
+    main()
